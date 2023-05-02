e606adf0152b644f8ebd36eda6344b6e194e3e27
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-05-02 09:18:13 +0600

    catagory.json

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000
+
+const chefs = require('./chefs.json')
+const foods = require('./foods.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('My Server Lets Go!!!!!!!!')
+})
+
+app.get('/foods', (req, res) =>{
+    res.send(foods)
+})
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
 })
 
 app.listen(port, () => {
