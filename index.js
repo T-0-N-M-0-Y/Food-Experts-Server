854350bc4561ade63b5ee2f461354340ab93704e
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-05-03 00:51:20 +0600

    Updates

--- index.js
+++ index.js
@@ -16,9 +16,17 @@ app.get('/', (req, res) => {
 app.get('/foods', (req, res) =>{
     res.send(foods)
 })
+
 app.get('/chefs', (req, res) =>{
     res.send(chefs)
 })
+
+app.get('/chefs/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedChefs = chefs.find(c => c.id == id);
+    res.send(selectedChefs);
+})
+
 app.get('/catagory', (req, res) =>{
     res.send(catagories)
 })
